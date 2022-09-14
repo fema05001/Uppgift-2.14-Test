38bdd329278c7d30b0c761e13d81119d692bd771
longf <longf@DESKTOP-2GJ1G8J>
2022-09-14 20:15:40 +0200

    Add project files.

---
+++ Uppgift 2.14/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Uppgift_2_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string x =Console.ReadLine();
+            string y = Console.ReadLine();
+            float a =float.Parse(x);
+            float b = float.Parse(y);
+            Console.Write("svaret blir");
+            Console.Write(a * b);
+        }
+    }
+}
